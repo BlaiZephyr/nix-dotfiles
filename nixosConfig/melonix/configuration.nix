c4a54225799baf45ff2957d01c229d08253b0d00
BlaiZephyr <arbeit.t.lechner@gmail.com>
2024-11-15 15:28:05 +0100

    yes i am.

--- nixosConfig/melonix/configuration.nix
+++ nixosConfig/melonix/configuration.nix
@@ -8,7 +8,7 @@
 }: {
   imports = [
     ./hardware-configuration.nix
-    ../../nixosModules
+    ../../modules/nixos
     inputs.home-manager.nixosModules.default
   ];
 
--- nixosConfig/melonix/home.nix
+++ nixosConfig/melonix/home.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   imports = [
-    ../../homeModules
+    ../../modules/home
   ];
 
   hm = {
