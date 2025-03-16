856be7db9adbf74732924b7c4e7301d9802f7982
dwilliam62 <don.e.williams@gmail.com>
2025-03-16 06:04:45 +0000

    Replace ghostty.nix it didn't have the key bindings Also added sample themes (commented out) set default to dracula to match zaneyos scheme

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -1,29 +1,83 @@
-_: {
+{pkgs, ...}: {
   programs.ghostty = {
-    enable = false;
-    enableBashIntegration = false;
-    enableZshIntegration = true;
-    settings = {
-      font-family = "JetBrainsMono Nerd Font Mono";
-      font-size = 14;
-      theme = "Dracula";
-      background-opacity = 0.95;
-      background-blur = 10;
-      font-thicken = false;
-      font-feature = ["ss04" "ss01"];
-      bold-is-bright = false;
-      adjust-box-thickness = 1;
-      cursor-style = "bar";
-      cursor-style-blink = false;
-      copy-on-select = false;
-      confirm-close-surface = false;
-      adjust-cursor-thickness = 1;
-      mouse-hide-while-typing = true;
-      window-padding-x = 4;
-      window-padding-y = 6;
-      window-padding-balance = true;
-      title = "GhosTTY";
-      gtk-single-instance = true;
-    };
+    enable = true;
+    package = pkgs.ghostty;
   };
+  home.file."./.config/ghostty/config".text = ''
+
+    #theme = Aura
+    theme = Dracula
+    #theme = Aardvark Blue
+    #theme = GruvboxDarkHard
+    adjust-cell-height = 10%
+    window-theme = dark
+    window-height = 32
+    window-width = 110
+    background-opacity = 0.95
+    background-blur-radius = 60
+    selection-background = #2d3f76
+    selection-foreground = #c8d3f5
+    cursor-style = bar
+    mouse-hide-while-typing = true
+
+    # keybindings
+    keybind = alt+s>r=reload_config
+    keybind = alt+s>x=close_surface
+
+    keybind = alt+s>n=new_window
+
+    # tabs
+    keybind = alt+s>c=new_tab
+    keybind = alt+s>shift+l=next_tab
+    keybind = alt+s>shift+h=previous_tab
+    keybind = alt+s>comma=move_tab:-1
+    keybind = alt+s>period=move_tab:1
+
+    # quick tab switch
+    keybind = alt+s>1=goto_tab:1
+    keybind = alt+s>2=goto_tab:2
+    keybind = alt+s>3=goto_tab:3
+    keybind = alt+s>4=goto_tab:4
+    keybind = alt+s>5=goto_tab:5
+    keybind = alt+s>6=goto_tab:6
+    keybind = alt+s>7=goto_tab:7
+    keybind = alt+s>8=goto_tab:8
+    keybind = alt+s>9=goto_tab:9
+
+    # split
+    keybind = alt+s>\=new_split:right
+    keybind = alt+s>-=new_split:down
+
+    keybind = alt+s>j=goto_split:bottom
+    keybind = alt+s>k=goto_split:top
+    keybind = alt+s>h=goto_split:left
+    keybind = alt+s>l=goto_split:right
+
+    keybind = alt+s>z=toggle_split_zoom
+
+    keybind = alt+s>e=equalize_splits
+
+    # other
+    #copy-on-select = clipboard
+
+    font-size = 12
+    #font-family = JetBrainsMono Nerd Font Mono
+    #font-family-bold = JetBrainsMono NFM Bold
+    #font-family-bold-italic = JetBrainsMono NFM Bold Italic
+    #font-family-italic = JetBrainsMono NFM Italic
+
+    font-family = BerkeleyMono Nerd Font
+    #font-family = Iosevka Nerd Font
+    # font-family = SFMono Nerd Font
+
+    title = "GhosTTY"
+
+    wait-after-command = false
+    shell-integration = detect
+    window-save-state = always
+    gtk-single-instance = true
+    unfocused-split-opacity = 0.5
+    quick-terminal-position = center
+    shell-integration-features = cursor,sudo
+  '';
 }
